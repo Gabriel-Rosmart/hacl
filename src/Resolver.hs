e07fe09f94296505129992b10246694123d94ddf
Gabriel Rosmart <gabrielrostron@gmail.com>
2025-09-17 13:10:44 +0200

    feat: prevented circular imports

--- src/Resolver.hs
+++ src/Resolver.hs
@@ -7,20 +7,33 @@ import qualified Data.Map as M
 import qualified Data.Text as T
 
 haclFromFile :: String -> IO Hacl
-haclFromFile filename = do 
-  contents <- readFile filename
-  case parse haclParser filename (T.pack contents) of
-    Left _ -> error "Could not parse document"
-    Right p -> return p
+haclFromFile filename = load [] filename
+
+-- Try to parse a given file if it is not in the banned list.
+-- A banned list is kept in order to ensure no circular imports happen
+
+load :: [String] -> String -> IO Hacl
+load banned filename = do
+  if (filename `elem` banned) then error ("Forbidden recursive import at file " ++ filename) else do
+    contents <- readFile filename
+    case parse haclParser filename (T.pack contents) of
+      Left _ -> error "Could not load file"
+      Right p -> resolveImports (banned ++ [filename]) p
 
 
-resolveImports :: Hacl -> IO Hacl
-resolveImports (HaclObject o) = do 
-  let intermediate = map resolvePair (M.toList o)
+-- Scan all pairs of an object and try to resolve imports if any
+
+resolveImports :: [String] -> Hacl -> IO Hacl
+resolveImports banned (HaclObject o) = do 
+  let intermediate = map (resolvePair banned) (M.toList o) :: [(T.Text, IO Hacl)]
   HaclObject <$> M.fromList <$> (sequenceA (map sequenceA intermediate))
-resolveImports _ = undefined
 
-resolvePair :: (T.Text, Hacl) -> (T.Text, IO Hacl)
-resolvePair (t, HaclObject o) = (t, resolveImports (HaclObject o))
-resolvePair (t, HaclImport i) = (t, haclFromFile (T.unpack i))
-resolvePair (t, e) = (t, return e)
+-- Only objects are allowed to have imports, trying to resolve
+-- any other type results in an error
+
+resolveImports _ _ = undefined
+
+resolvePair :: [String] -> (T.Text, Hacl) -> (T.Text, IO Hacl)
+resolvePair banned (t, HaclObject o) = (t, resolveImports banned (HaclObject o))
+resolvePair banned (t, HaclImport i) = (t, load banned (T.unpack i))
+resolvePair _ (t, e) = (t, return e)
