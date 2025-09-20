f1bd94ad1fbad83aa7ec4703ee264a95eb2665b9
Gabriel Rosmart <gabrielrostron@gmail.com>
2025-09-21 00:44:25 +0200

    feat: moved pretty printing to own module

---
+++ src/Data/Hacl/Pretty.hs
@@ -0,0 +1,59 @@
+module Data.Hacl.Pretty (pretty, prettyM) where 
+ 
+import Data.Hacl.Types
+import Data.List
+import qualified Data.List.NonEmpty as NE
+import qualified Data.Map as M
+import qualified Data.Text as T
+
+-- Exported function
+
+pretty :: Hacl -> String
+pretty h = ppHacl False 0 h
+
+prettyM :: Maybe Hacl -> String
+prettyM Nothing = "[Nothing Hacl]"
+prettyM (Just h) = "Just " ++ pretty h
+
+-- Inner Pretty Print functions
+
+ppHacl :: Bool -> Int -> Hacl -> String 
+
+ppHacl isObjectValue n (HaclObject o) = (ident ++ "{\n")
+  ++
+    (
+      intercalate ", \n" (map (\p -> ident_val ++ (quote $ T.unpack (fst p)) ++ " : " ++ (ppHacl True (n + 1) $ snd p)) (M.toList o))
+    )
+  ++ ("\n" ++ ((concat $ replicate (n * 4) " ")) ++ "}")
+
+  where ident = identKV isObjectValue n
+        ident_val = (concat $ replicate ((n + 1) * 4) " ")
+
+ppHacl isObjectValue n (HaclArray a) = (identf ++ "[\n")
+  ++
+    (
+      intercalate ", \n" (NE.toList $ NE.map (ppHacl (not isObjectValue) $ n + 1) a)
+    )
+  ++ ("\n" ++ identl ++ "]")
+
+  where identf = identKV isObjectValue n
+        identl = identKV (not isObjectValue) n
+
+
+ppHacl b n (HaclNumber h) = (identKV b n) ++ (ppHaclNumber h)
+ppHacl b n (HaclText t) = (identKV b n) ++ (quote $ T.unpack t)
+ppHacl x n (HaclBool b) = (identKV x n) ++ (show b)
+ppHacl b n (HaclNothing) = (identKV b n)  ++ "nothing"
+ppHacl b n (HaclImport i) = (identKV b n) ++ "import " ++ (quote $ T.unpack i)
+
+ppHaclNumber :: HaclNumberType -> String
+ppHaclNumber (HaclDouble d) = show d 
+ppHaclNumber (HaclInteger i) = show i
+
+-- If is an object value do not ident, otherwise ident
+identKV :: Bool -> Int -> String
+identKV False indentlvl = (concat $ replicate (indentlvl * 4) " ")
+identKV True _ = ""
+
+quote :: String -> String
+quote s = "\"" ++ s ++ "\""
--- src/Data/Hacl/Types.hs
+++ src/Data/Hacl/Types.hs
@@ -2,7 +2,6 @@ module Data.Hacl.Types where
 
 import qualified Data.Text as T
 import Data.Scientific
-import Data.List
 import Data.Maybe (fromMaybe)
 import qualified Data.List.NonEmpty as NE
 import qualified Data.Map as M
@@ -14,56 +13,12 @@ data Hacl = HaclText T.Text |
             HaclArray (NE.NonEmpty Hacl) |
             HaclObject (M.Map T.Text Hacl) |
             HaclImport T.Text
-            deriving Eq
+            deriving (Eq, Show)
 
-data HaclNumberType = HaclInteger Int | HaclDouble Double deriving Eq
+data HaclNumberType = HaclInteger Int | HaclDouble Double deriving (Eq, Show)
 
 castHaclNumber :: Scientific -> HaclNumberType
 castHaclNumber n | (isFloating n) = HaclDouble (toRealFloat n)
                  | otherwise = HaclInteger (fromMaybe 0 (toBoundedInteger n))
 
--- Pretty Printing --
 
-instance Show HaclNumberType where
-  show (HaclDouble d) = show d 
-  show (HaclInteger i) = show i
-
-instance Show Hacl where
-  show h = ppHacl False 0 h
-
-ppHacl :: Bool -> Int -> Hacl -> String 
-
-ppHacl isObjectValue n (HaclObject o) = (ident ++ "{\n")
-  ++
-    (
-      intercalate ", \n" (map (\p -> ident_val ++ (quote $ T.unpack (fst p)) ++ " : " ++ (ppHacl True (n + 1) $ snd p)) (M.toList o))
-    )
-  ++ ("\n" ++ ((concat $ replicate (n * 4) " ")) ++ "}")
-
-  where ident = identKV isObjectValue n
-        ident_val = (concat $ replicate ((n + 1) * 4) " ")
-
-ppHacl isObjectValue n (HaclArray a) = (identf ++ "[\n")
-  ++
-    (
-      intercalate ", \n" (NE.toList $ NE.map (ppHacl (not isObjectValue) $ n + 1) a)
-    )
-  ++ ("\n" ++ identl ++ "]")
-
-  where identf = identKV isObjectValue n
-        identl = identKV (not isObjectValue) n
-
-
-ppHacl b n (HaclNumber h) = (identKV b n) ++ (show h)
-ppHacl b n (HaclText t) = (identKV b n) ++ (quote $ T.unpack t)
-ppHacl x n (HaclBool b) = (identKV x n) ++ (show b)
-ppHacl b n (HaclNothing) = (identKV b n)  ++ "nothing"
-ppHacl b n (HaclImport i) = (identKV b n) ++ "import " ++ (quote $ T.unpack i)
-
--- If is an object value do not ident, otherwise ident
-identKV :: Bool -> Int -> String
-identKV False indentlvl = (concat $ replicate (indentlvl * 4) " ")
-identKV True _ = ""
-
-quote :: String -> String
-quote s = "\"" ++ s ++ "\""
