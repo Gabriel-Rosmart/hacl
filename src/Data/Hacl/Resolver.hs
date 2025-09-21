7ce4f898e28f70a0d3914a5fda74ce9907eeb1d4
Gabriel Rosmart <gabrielrostron@gmail.com>
2025-09-21 13:56:39 +0200

    feat: better error reporting when failing to parse and recursive imports

--- src/Data/Hacl/Resolver.hs
+++ src/Data/Hacl/Resolver.hs
@@ -7,33 +7,33 @@ import qualified Data.Map as M
 import qualified Data.Text as T
 
 haclFromFile :: String -> IO Hacl
-haclFromFile filename = load [] filename
+haclFromFile filename = load [] filename filename
 
 -- Try to parse a given file if it is not in the banned list.
 -- A banned list is kept in order to ensure no circular imports happen
 
-load :: [String] -> String -> IO Hacl
-load banned filename = do
-  if (filename `elem` banned) then error ("Forbidden recursive import at file " ++ filename) else do
+load :: [String] -> String -> String -> IO Hacl
+load banned caller filename = do
+  if (filename `elem` banned) then error ("Forbidden recursive import, file " ++ caller ++ " tried to import " ++ filename) else do
     contents <- readFile filename
     case parse haclParser filename (T.pack contents) of
-      Left _ -> error "Could not load file"
-      Right p -> resolveImports (banned ++ [filename]) p
+      Left e -> error (filename ++ " failed to parse with the following error:\n" ++ errorBundlePretty e)
+      Right p -> resolveImports (banned ++ [filename]) filename p
 
 
 -- Scan all pairs of an object and try to resolve imports if any
 
-resolveImports :: [String] -> Hacl -> IO Hacl
-resolveImports banned (HaclObject o) = do 
-  let intermediate = map (resolvePair banned) (M.toList o) :: [(T.Text, IO Hacl)]
+resolveImports :: [String] -> String -> Hacl -> IO Hacl
+resolveImports banned caller (HaclObject o) = do 
+  let intermediate = map (resolvePair banned caller) (M.toList o) :: [(T.Text, IO Hacl)]
   HaclObject <$> M.fromList <$> (sequenceA (map sequenceA intermediate))
 
 -- Only objects are allowed to have imports, trying to resolve
 -- any other type results in an error
 
-resolveImports _ _ = undefined
+resolveImports _ _ _ = error "Only objects can resolve imports"
 
-resolvePair :: [String] -> (T.Text, Hacl) -> (T.Text, IO Hacl)
-resolvePair banned (t, HaclObject o) = (t, resolveImports banned (HaclObject o))
-resolvePair banned (t, HaclImport i) = (t, load banned (T.unpack i))
-resolvePair _ (t, e) = (t, return e)
+resolvePair :: [String] -> String -> (T.Text, Hacl) -> (T.Text, IO Hacl)
+resolvePair banned caller (t, HaclObject o) = (t, resolveImports banned caller (HaclObject o))
+resolvePair banned caller (t, HaclImport i) = (t, load banned caller (T.unpack i))
+resolvePair _ _ (t, e) = (t, return e)
