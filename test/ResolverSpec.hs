e99b620c20a15e35d8e2998abc56a787adb73a5f
Gabriel Rosmart <gabrielrostron@gmail.com>
2025-09-21 21:51:47 +0200

    feat: added some IO tests

--- test/ResolverSpec.hs
+++ test/ResolverSpec.hs
@@ -2,18 +2,28 @@
 
 module ResolverSpec where
 
-import Text.Megaparsec
 import Test.Hspec
-import Test.Hspec.Megaparsec
-import Data.Hacl.Parser
 import Data.Hacl.Types
-import qualified Data.List.NonEmpty as NE
+import Data.Hacl.Resolver
 import qualified Data.Text as T
 import qualified Data.Map as M
 
 spec :: Spec
 
 spec = do
-  describe "Hacl Resolver Dummy" $ do 
-    it "Parses true" $ parse hbool "" "true" `shouldParse` (HaclBool True)
-
+  describe "IO Hacl" $ do 
+    it "Parses file with no imports" $
+      (haclFromFile "test/assets/single.hacl") `shouldReturn` (HaclObject $ M.fromList [(T.pack "single", HaclBool True)])
+    
+    it "Parses file with imports" $
+      (haclFromFile "test/assets/wimport.hacl")
+        `shouldReturn` (HaclObject $ M.fromList [(T.pack "import", HaclObject $ M.fromList [(T.pack "single", HaclBool True)])])
+        
+  describe "Failing IO Hacl" $ do 
+    it "Cannot import unexisting file" $
+      (haclFromFile "test/assets/unexisting.hacl") `shouldThrow` anyException
+    
+    it "Prevents recursive import to same file" $  
+      (haclFromFile "test/assets/recursive.hacl") `shouldThrow` anyErrorCall
+    
+    it "Prevents recursive imports between files" $ (haclFromFile "test/assets/recursive_between1.hacl") `shouldThrow` anyErrorCall 
