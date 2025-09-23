6cb3fe720ffcd592da326d76c69abcb1eedcb94b
Gabriel Rosmart <gabrielrostron@gmail.com>
2025-09-23 12:38:33 +0200

    ref: resolver module now a bit clearer to read

--- src/Data/Hacl/Resolver.hs
+++ src/Data/Hacl/Resolver.hs
@@ -5,13 +5,11 @@ import Data.Hacl.Parser
 import Data.Hacl.Types
 import qualified Data.Map as M
 import qualified Data.Text as T
-import Control.Monad.Except
+import Control.Monad.Trans.Except
 import Control.Monad.Trans.Class(lift)
 import Data.Hacl.Error
 import System.Directory
 
-type IOHacl = ExceptT HaclError IO Hacl
-type IOString = ExceptT HaclError IO String
 type FileList = [FilePath]
 
 haclFromFile :: FilePath -> IO (Either HaclError Hacl) 
@@ -20,7 +18,7 @@ haclFromFile filename = runExceptT (load [] filename filename)
 -- Try to parse a given file if it is not in the banned list.
 -- A banned list is kept in order to ensure no circular imports happen
 
-load :: FileList -> FilePath -> FilePath -> IOHacl
+load :: FileList -> FilePath -> FilePath -> ExceptT HaclError IO Hacl
 load banned ccaller filename = do
   if (filename `elem` banned) then ExceptT (return $ Left (RecursiveImport ccaller filename)) else do 
     (tryReadFile filename) >>= (\contents -> 
@@ -30,25 +28,25 @@ load banned ccaller filename = do
       )
 
 
--- Scan all pairs of an object and try to resolve imports if any
+-- Scan all pairs of an object and try to resolve imports if any.
+-- Only objects are allowed to have imports, trying to resolve any other type results in an error
 
-resolveImports :: FileList -> FilePath -> Hacl -> IOHacl
-resolveImports banned ccaller (HaclObject o) = do 
-  let intermediate = map (resolvePair banned ccaller) (M.toList o) :: [(T.Text, IOHacl)]
-  HaclObject <$> M.fromList <$> (sequenceA (map sequenceA intermediate))
+resolveImports :: FileList -> FilePath -> Hacl -> ExceptT HaclError IO Hacl
+resolveImports banned ccaller (HaclObject object) = HaclObject <$> M.fromList <$>
+  (sequenceA (map (resolvePair banned ccaller) (M.toList object)))
 
--- Only objects are allowed to have imports, trying to resolve
--- any other type results in an error
+resolveImports _ _ _ = error "Data.Hacl.Error <resolveImports> function cannot be called to anything other than an object"
 
-resolveImports _ _ _ = error "Only objects can resolve imports"
+-- Try to resolve imports of a pair if any
 
-resolvePair :: FileList -> FilePath -> (T.Text, Hacl) -> (T.Text, IOHacl)
-resolvePair banned ccaller (t, HaclObject o) = (t, resolveImports banned ccaller (HaclObject o))
-resolvePair banned ccaller (t, HaclImport i) = (t, load banned ccaller (T.unpack i))
-resolvePair _ _ (t, e) = (t, return e)
+resolvePair :: FileList -> FilePath -> (T.Text, Hacl) -> ExceptT HaclError IO (T.Text, Hacl)
+resolvePair banned ccaller (t, HaclObject o) = sequenceA (t, resolveImports banned ccaller (HaclObject o))
+resolvePair banned ccaller (t, HaclImport i) = sequenceA (t, load banned ccaller (T.unpack i))
+resolvePair _ _ (t, e) = return (t, e)
 
+-- Try to read a file failing if it does not exist
 
-tryReadFile :: FilePath -> IOString
+tryReadFile :: FilePath -> ExceptT HaclError IO String
 tryReadFile file = (lift $ doesFileExist file) >>= (\b -> if b == False then
     ExceptT (return $ Left (ResourceNotFound file)) else
     (lift $ readFile file) >>= (\c -> ExceptT (return $ Right c))
